c98ba795a7c4374ac61176ab7594ff09c7bfbef6
Ahmed Shehata <ahmedshehataq98@gmail.com>
2023-12-04 13:12:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": "https://chatman-api.fly.dev",
-  //   },
-  // },
-
   plugins: [react()],
 });
